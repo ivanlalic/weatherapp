3269612f44df81b8ea9878f4c5a87c71109d6cd1
ivanlalic <ivanlalic@gmail.com>
2020-08-11 01:23:50 -0300

    Updates

---
+++ precache-manifest.00912c8931afe2ed95b6fab92afd46ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e420b1f22c45ea45766c670fe69ba0b",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "7f9e028030473b2645ac",
+    "url": "/weatherapp/static/css/main.3e82f7ce.chunk.css"
+  },
+  {
+    "revision": "bc1cabacc8cb88bf9cf4",
+    "url": "/weatherapp/static/js/2.903f7480.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.903f7480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f9e028030473b2645ac",
+    "url": "/weatherapp/static/js/main.77cb4459.chunk.js"
+  },
+  {
+    "revision": "97994ece5cf1ba8fd757",
+    "url": "/weatherapp/static/js/runtime-main.80ae9ae2.js"
+  }
+]);
